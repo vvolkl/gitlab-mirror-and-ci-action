6ea9c3127633ef44fadf62b38def059dcbaf5900
Valentin Volkl <valentin.volkl@cern.ch>
2020-06-04 15:11:52 +0200

    up3

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,20 +7,18 @@ POLL_TIMEOUT=${POLL_TIMEOUT:-$DEFAULT_POLL_TIMEOUT}
 
 git checkout "${GITHUB_REF:11}"
 
-branch=${GITHUB_REPOSITORY}/$(git symbolic-ref --short HEAD)
+#branch=${GITHUB_REPOSITORY}/$(git symbolic-ref --short HEAD)
 shortbranch=$(git symbolic-ref --short HEAD)
 
 sh -c "git config --global credential.username $GITLAB_USERNAME"
 sh -c "git config --global core.askPass /cred-helper.sh"
 sh -c "git config --global credential.helper cache"
 sh -c "git remote add mirror $*"
-sh -c "echo pushing to $branch branch at $(git remote get-url --push mirror)"
+sh -c "echo pushing to $shortbranch branch at $(git remote get-url --push mirror)"
 sh -c "git push mirror $shortbranch"
 
 sleep $POLL_TIMEOUT
 
-# convert slashes in a HTML-compatible way
-branch=${branch//\//%2F}
 
 pipeline_id=$(curl --header "PRIVATE-TOKEN: $GITLAB_PASSWORD" --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/repository/commits/${shortbranch}" | jq '.last_pipeline.id')
 
