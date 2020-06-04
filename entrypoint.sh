c2f9fddd87446e5a7463304c4f4b215d12cdfe65
Valentin Volkl <valentin.volkl@cern.ch>
2020-06-04 14:24:59 +0200

    fix branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,14 @@ POLL_TIMEOUT=${POLL_TIMEOUT:-$DEFAULT_POLL_TIMEOUT}
 git checkout "${GITHUB_REF:11}"
 
 branch=${GITHUB_REPOSITORY}/$(git symbolic-ref --short HEAD)
+shortbranch=$(git symbolic-ref --short HEAD)
 
 sh -c "git config --global credential.username $GITLAB_USERNAME"
 sh -c "git config --global core.askPass /cred-helper.sh"
 sh -c "git config --global credential.helper cache"
 sh -c "git remote add mirror $*"
 sh -c "echo pushing to $branch branch at $(git remote get-url --push mirror)"
-sh -c "git push mirror $branch"
+sh -c "git push mirror $shortbranch"
 
 sleep $POLL_TIMEOUT
 
